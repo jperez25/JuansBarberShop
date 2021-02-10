edb9370bc87f9993d02067c1ddd6ac78f109b9f6
jperez25 <jova9991@gmail.com>
2021-02-10 17:54:27 -0600

    nav bar and home component

--- juanBarberShop/src/app/app.module.ts
+++ juanBarberShop/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
